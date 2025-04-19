9a65a471f2df226a017369bfc70598a3465b3a1d
Philipp Kempf <philippkempf@MacBook-Air-von-Philipp.local>
2025-04-19 16:37:15 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// POST-Route für das Kontaktformular
+app.post('/kontakt', (req, res) => {
+  const { name, email, nachricht } = req.body;
+
+  // Simples Logging in eine Textdatei (anstatt Datenbank)
+  const eintrag = `Name: ${name}, E-Mail: ${email}, Nachricht: ${nachricht}\n`;
+  fs.appendFileSync('kontakte.txt', eintrag);
+
+  res.send(`<h2>Danke für deine Nachricht, ${name}!</h2><a href="/">Zurück</a>`);
+});
+
+// Server starten
+app.listen(PORT, () => {
+  console.log(`🚀 Server läuft auf http://localhost:${PORT}`);
+});
